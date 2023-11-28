acb1b96bbfa35491d71b0ee73a64e0b214b0320f
b.zakaria <zakaria_bouzangad@gmail.Com>
2023-11-28 11:43:44 +0100

    project capstone

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="categories-container">
+      <div className="categorie-container">
+      {/* <img /> */}
+        <div className="cateforie-body-container">
+          <h2>Hats</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+      <div className="categorie-container">
+      {/* <img /> */}
+        <div className="cateforie-body-container">
+          <h2>Jackets</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+      <div className="categorie-container">
+      {/* <img /> */}
+        <div className="cateforie-body-container">
+          <h2>Sneakers</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+      <div className="categorie-container">
+      {/* <img /> */}
+        <div className="cateforie-body-container">
+          <h2>Womens</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+      <div className="categorie-container">
+      {/* <img /> */}
+        <div className="cateforie-body-container">
+          <h2>Mens</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
     </div>
   );
 }
