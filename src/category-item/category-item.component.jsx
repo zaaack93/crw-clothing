76f9e56ab69240950f278be8caecb38e8e53daa8
b.zakaria <zakaria_bouzangad@gmail.Com>
2023-11-28 13:20:03 +0100

    Category Item Component

---
+++ src/category-item/category-item.component.jsx
@@ -0,0 +1,21 @@
+import "./category-item.style.scss";
+
+const CategoryItem = ({ category }) => {
+  return (
+    <div key={category.id} className="category-container">
+      {/* <img /> */}
+      <div
+        className="background-image"
+        style={{
+          backgroundImage: `url(${category.imageUrl})`,
+        }}
+      />
+      <div className="category-body-container">
+        <h2>{category.title}</h2>
+        <p>Shop Now</p>
+      </div>
+    </div>
+  );
+};
+
+export default CategoryItem;
