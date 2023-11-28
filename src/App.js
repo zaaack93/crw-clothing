98040d6f4febcd216e2dce7cf8bed438692e1578
b.zakaria <zakaria_bouzangad@gmail.Com>
2023-11-28 11:56:08 +0100

    Add customs items

--- src/App.js
+++ src/App.js
@@ -1,43 +1,40 @@
 const App = () => {
+  const categories = [
+    {
+      id: 1,
+      title: "Hats",
+    },
+    {
+      id: 2,
+      title: "Jackets",
+    },
+    {
+      id: 3,
+      title: "Sneakers",
+    },
+    {
+      id: 4,
+      title: "Womens",
+    },
+    {
+      id: 5,
+      title: "Mens",
+    },
+  ];
   return (
     <div className="categories-container">
-      <div className="categorie-container">
-      {/* <img /> */}
-        <div className="cateforie-body-container">
-          <h2>Hats</h2>
-          <p>Shop Now</p>
+      {categories.map((categorie) => (
+        <div className="categorie-container">
+          {/* <img /> */}
+          <div className="background-image" />
+          <div className="cateforie-body-container">
+            <h2>{categorie.title}</h2>
+            <p>Shop Now</p>
+          </div>
         </div>
-      </div>
-      <div className="categorie-container">
-      {/* <img /> */}
-        <div className="cateforie-body-container">
-          <h2>Jackets</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-      <div className="categorie-container">
-      {/* <img /> */}
-        <div className="cateforie-body-container">
-          <h2>Sneakers</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-      <div className="categorie-container">
-      {/* <img /> */}
-        <div className="cateforie-body-container">
-          <h2>Womens</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-      <div className="categorie-container">
-      {/* <img /> */}
-        <div className="cateforie-body-container">
-          <h2>Mens</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
+      ))}
     </div>
   );
-}
+};
 
 export default App;
