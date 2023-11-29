14713530e76752c5d3cc9ec3a59068f7439f1bcc
b.zakaria <zakaria_bouzangad@gmail.Com>
2023-11-29 15:00:20 +0100

    Implimentation navigation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Router, Routes } from "react-router-dom";
 import Home from "./routes/home/home.component";
+import Navigation from "./routes/navigation/navigation.component";
+
+const Shop = () =>{
+  return (
+    <div>
+      I'm shop component
+    </div>
+  )
+}
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path="shop" element={<Shop />} />
+      </Route>
     </Routes>
   );
 };
---
+++ src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,25 @@
+import { Fragment } from "react";
+import { Link, Outlet } from "react-router-dom"
+
+import { ReactComponent as CrownLogo } from "../../assets/crown.svg";
+
+import './navigation.style.scss'
+
+const Navigation = () =>{
+    return (
+        <Fragment>
+            <div className="navigation">
+                
+                <Link className="logo-container" to="/">
+                   <CrownLogo className="logo" />
+                </Link>
+                <div className="nav-links-container">
+                    <Link to="/shop" className="nav-link">SHOP</Link>
+                </div>
+            </div>
+            <Outlet></Outlet>
+        </Fragment>
+    )
+}
+
+export default Navigation;
