7c0ac9aa545025398219f92664acc087d14916fd
b.zakaria <zakaria_bouzangad@gmail.Com>
2023-11-30 10:12:15 +0100

    add sign in page

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -15,6 +15,7 @@ const Navigation = () =>{
                 </Link>
                 <div className="nav-links-container">
                     <Link to="/shop" className="nav-link">SHOP</Link>
+                    <Link to="/sign-in" className="nav-link">SIGN IN</Link>
                 </div>
             </div>
             <Outlet></Outlet>
---
+++ src/routes/sign-in/sign-in.component.jsx
@@ -0,0 +1,9 @@
+const SignIn = () => {
+    return (
+        <div>
+            sign in component
+        </div>
+    );
+};
+
+export default SignIn;
