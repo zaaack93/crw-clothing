613a6796f7b562533c49bceef5129dfd4f834d2f
b.zakaria <zakaria_bouzangad@gmail.Com>
2023-11-30 13:33:04 +0100

    remove the redirect implimentation because we don't need it

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,11 +1,8 @@
-import { getRedirectResult } from "firebase/auth";
 import {
   createUserDocumentFromAuth,
-  signInWithGoogleRedirect,
   signInWithGooglePopup,
   auth,
 } from "../../utils/firebase/firebase.utils";
-import { useEffect } from "react";
 
 const SignIn = () => {
   const signIn = async () => {
@@ -14,26 +11,10 @@ const SignIn = () => {
     const userRefDoc = await createUserDocumentFromAuth(user);
     console.log(userRefDoc);
   };
-
-  const chechRedirectionState = async () => {
-    //auth is singleton it keep track of authentication fro the whole app
-    const response = await getRedirectResult(auth);
-    if(response){
-        const userRefDoc = await createUserDocumentFromAuth(response.user);
-        console.log(userRefDoc);
-    }
-  };
-
-  useEffect(() => {
-    chechRedirectionState();
-  }, []);
   return (
     <div>
       sign in component
       <button onClick={signIn}>Sign in with google popup</button>
-      <button onClick={signInWithGoogleRedirect}>
-        Sign in with google redirect
-      </button>
     </div>
   );
 };
